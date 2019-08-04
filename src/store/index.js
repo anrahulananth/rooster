3bdc10d976f6cc1e20534990778d08d5d4e7ace2
Rahul Anantharama <rahul.ananthrama@gmail.com>
2019-08-04 17:54:28 +0530

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -11,8 +11,7 @@ const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
     reducer, 
     compose(
-        applyMiddleware(logger, sagaMiddleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        applyMiddleware(logger, sagaMiddleware)
     )
 );
 
